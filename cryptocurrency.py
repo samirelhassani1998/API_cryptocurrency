a1976da61be220930896877966e17793073d075f
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-20 23:09:20 +0100

    Update cryptocurrency.py

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -1,33 +1,33 @@
 import streamlit as st
 import requests
 
-# Set up the API endpoint and headers
-api_endpoint = "https://rest.coinapi.io/v1/exchangerate/{}/{}"
-headers = {"X-CoinAPI-Key": "Y6A1A2057-BABF-49F7-A562-FCF109B6BD2E"}
+# Set API endpoint and headers
+url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
+headers = {
+    "Accepts": "application/json",
+    "X-CMC_PRO_API_KEY": "c7271d78-54dd-4159-8409-6486b5f01e27"
+}
 
-# Define the cryptocurrencies and fiat currencies to retrieve prices for
-cryptocurrencies = ["BTC", "ETH", "XRP"]
-fiat_currencies = ["USD", "EUR", "GBP"]
+# Set parameters for API request
+params = {
+    "start": "1",
+    "limit": "10",
+    "convert": "USD"
+}
 
-# Create a list to store the retrieved prices
-prices = []
+# Make API request
+response = requests.get(url, headers=headers, params=params)
 
-# Loop through each cryptocurrency and fiat currency combination, retrieve the price,
-# and add it to the list of prices
-for crypto in cryptocurrencies:
-    for fiat in fiat_currencies:
-        url = api_endpoint.format(crypto, fiat)
-        response = requests.get(url, headers=headers)
-        data = response.json()
-        print("Code d'état de la réponse :", response.status_code)
-        print("Texte de la réponse :", response.text)
-        print("Données JSON :", data)
-        prices.append((crypto, fiat, data["rate"]))
-
-# Create a Streamlit app to display the prices
-st.title("Cryptocurrency Prices")
-
-# Create a table to display the prices
-table_header = ["Cryptocurrency", "Fiat Currency", "Price"]
-table_data = [[crypto, fiat, price] for crypto, fiat, price in prices]
-st.table(table_data, header=table_header)
+# Check if API request is successful
+if response.status_code != 200:
+    st.error("Failed to retrieve data from CoinMarketCap API - check your API Key and try again.")
+else:
+    # Parse API response and extract relevant data
+    data = response.json()["data"]
+    coin_names = [coin["name"] for coin in data]
+    coin_prices = [coin["quote"]["USD"]["price"] for coin in data]
+    
+    # Display data using Streamlit
+    st.write("# Top 10 Cryptocurrencies by Market Cap")
+    for i in range(len(coin_names)):
+        st.write(f"{coin_names[i]}: {coin_prices[i]:,.2f} USD")
