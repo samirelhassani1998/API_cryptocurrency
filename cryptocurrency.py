ce3f73ec058c40bfc66ea7d450fb7ee75e65b54c
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-22 22:13:47 +0100

    Update cryptocurrency.py

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -3,34 +3,44 @@ import requests
 import pandas as pd
 import numpy as np
 import time
+import datetime
+import matplotlib.pyplot as plt
 
 # Set API endpoint and headers
-url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
-headers = {
-    "Accepts": "application/json",
-    "X-CMC_PRO_API_KEY": "c7271d78-54dd-4159-8409-6486b5f01e27"
-}
+url = "https://api.binance.com/api/v3/ticker/price"
+symbol = "BTCUSDT"
 
-# Set parameters for API request
-params = {
-    "symbol": "BTC",
-    "convert": "USD"
-}
+# Time interval in seconds
+time_interval = 5
 
-# Function to make API request and retrieve price data
-def get_btc_price():
-    response = requests.get(url, headers=headers, params=params)
-    data = response.json()["data"]["BTC"]
-    price = data["quote"]["USD"]["price"]
-    return price
+# Set up empty lists for timestamp and price data
+timestamps = []
+prices = []
 
-# Streamlit app
-st.title("Current BTC Price")
-price = get_btc_price()
-st.write(f"Current BTC price is {price:.2f} USD")
+# Set up plot
+fig, ax = plt.subplots()
+ax.set_title("BTC/USDT Price")
 
-# Streamlit app loop to update price every minute
+# Main program loop
 while True:
-    time.sleep(60)
-    price = get_btc_price()
-    st.write(f"Current BTC price is {price:.2f} USD")
+    # Make API request
+    params = {"symbol": symbol}
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        st.error("Failed to retrieve data from Binance API - check your API Key and try again.")
+        break
+    data = response.json()
+    price = float(data["price"])
+    timestamp = datetime.datetime.now()
+    timestamps.append(timestamp)
+    prices.append(price)
+    # Update plot
+    ax.plot(timestamps, prices, color="blue")
+    ax.set_xlim(timestamps[0], timestamps[-1])
+    ax.set_ylim(min(prices), max(prices))
+    fig.canvas.draw()
+    # Wait for time interval
+    time.sleep(time_interval)
+
+# Show plot
+st.pyplot(fig)
