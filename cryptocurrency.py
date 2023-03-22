e19e1c137a44d57b6dcc39aad3210552e2a3df21
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-03-22 22:09:17 +0100

    Update cryptocurrency.py

--- cryptocurrency.py
+++ cryptocurrency.py
@@ -1,30 +1,36 @@
 import streamlit as st
 import requests
 import pandas as pd
+import numpy as np
 import time
 
 # Set API endpoint and headers
-url = "https://api.coingecko.com/api/v3/simple/price"
-params = {
-    "ids": "bitcoin",
-    "vs_currencies": "usd"
+url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+headers = {
+    "Accepts": "application/json",
+    "X-CMC_PRO_API_KEY": "c7271d78-54dd-4159-8409-6486b5f01e27"
 }
 
-# Define function to get BTC price
-def get_btc_price():
-    response = requests.get(url, params=params)
-    if response.status_code != 200:
-        return None
-    else:
-        data = response.json()
-        return data["bitcoin"]["usd"]
+# Set parameters for API request
+params = {
+    "symbol": "BTC",
+    "convert": "USD"
+}
 
-# Display BTC price in real-time
-st.write("# Real-Time BTC Price")
+# Function to make API request and retrieve price data
+def get_btc_price():
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()["data"]["BTC"]
+    price = data["quote"]["USD"]["price"]
+    return price
+
+# Streamlit app
+st.title("Current BTC Price")
+price = get_btc_price()
+st.write(f"Current BTC price is {price:.2f} USD")
+
+# Streamlit app loop to update price every minute
 while True:
+    time.sleep(60)
     price = get_btc_price()
-    if price:
-        st.write(f"BTC price: {price} USD")
-    else:
-        st.error("Failed to retrieve BTC price from CoinGecko API - check your internet connection.")
-    time.sleep(5)
+    st.write(f"Current BTC price is {price:.2f} USD")
